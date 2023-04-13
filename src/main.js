93ba7cc2e16fd6c67709e3f6ff320fdf6f8159a0
Diji Durojaiye <akindijijaiyejaiye@gmail.com>
2023-04-13 14:31:11 +0100

    add a new object main.js file

--- src/main.js
+++ src/main.js
@@ -4,3 +4,11 @@ import './css/styles.css';
 import $ from 'jquery';
 
 let simon = new Simon;
+
+//Press key to start
+/* green = 0, red= 1, yellow =2, blue=3 */
+$(document).on("keydown", function (element) {
+    if (element.keyCode === 13) {
+      simon.nextSequence();
+    }
+  });
